3f953f350b4d6b6e6b7b47f2508bb5b2b2142c58
Songzhou Yang <yangsongzhou@outlook.com>
2017-01-12 00:02:37 +0800

    colorful print

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Environment
+import System.Console.ANSI
 import Text.Parsec (parse)
 import Parser
 
@@ -8,6 +9,12 @@ main :: IO ()
 main = do
   args <- getArgs
   let expr = head args
-  print $ case parse parseExpr "lisp" expr of
-    Left err -> "No match:" `mappend` show err
-    Right val -> show val
+  case parse parseExpr "lisp" expr of
+    Left err -> do
+                setSGR [SetColor Foreground Vivid Red]
+                putStr $ "No match:" `mappend` show err
+                setSGR [Reset]
+    Right val -> do
+                 setSGR [SetColor Foreground Vivid Blue]
+                 putStr $ show val
+                 setSGR [Reset]
