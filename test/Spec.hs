7c2ffc0ad8abb217dda3dae71320a692de0fb06c
Songzhou Yang <yangsongzhou@outlook.com>
2017-01-14 15:10:33 +0800

    fix lint and add test + *

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,10 +8,25 @@ import Interpreter.Operand
 eps :: Double
 eps = 1e-5
 
-prop_eval :: Positive Integer -> Property
-prop_eval (Positive n) = monadicIO $ do
-  value <- run $ evalAnyWay (show n)
+prop_number :: Positive Integer -> Property
+prop_number (Positive n) = monadicIO $ do
+  value <- run $ evalAnyWay $ show n
   res <- liftIO . unwrapIOThrows . liftThrows $ unwrapNumber value
-  assert (abs (res - (fromInteger n)) < eps)
+  assert (abs res - fromInteger n < eps)
 
-main = quickCheck prop_eval
+prop_add :: Positive Double -> Positive Double -> Property
+prop_add (Positive x) (Positive y) = monadicIO $ do
+  value <- run $ evalAnyWay $ concat ["(+ ", show x, " ", show y, ")"]
+  res <- liftIO . unwrapIOThrows . liftThrows $ unwrapNumber value
+  assert (abs res - (x + y) < eps)
+
+prop_mul :: Positive Double -> Positive Double -> Property
+prop_mul (Positive x) (Positive y) = monadicIO $ do
+  value <- run $ evalAnyWay $ concat ["(* ", show x, " ", show y, ")"]
+  res <- liftIO . unwrapIOThrows . liftThrows $ unwrapNumber value
+  assert (abs res - (x * y) < eps)
+
+main = do
+  quickCheck prop_number
+  quickCheck prop_add
+  quickCheck prop_mul
