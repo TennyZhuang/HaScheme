4b6ccfed5bbe9e606aae1f90172ba07d77dde168
Songzhou Yang <yangsongzhou@outlook.com>
2017-01-14 16:01:04 +0800

    modify assert and add test hiherorder function

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,32 +1,43 @@
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
+import Control.Monad
 import Control.Monad.IO.Class (liftIO)
 import Repl
 import Interpreter.Define
 import Interpreter.Operand
 
-eps :: Double
-eps = 1e-5
+newtype SmallInt = SmallInt { unWrap :: Integer } deriving (Show)
+
+instance Arbitrary SmallInt where
+  arbitrary  = SmallInt `liftM` choose (1, 20)
 
 prop_number :: Positive Integer -> Property
 prop_number (Positive n) = monadicIO $ do
   value <- run $ evalAnyWay $ show n
   res <- liftIO . unwrapIOThrows . liftThrows $ unwrapNumber value
-  assert (abs res - fromInteger n < eps)
+  assert (res == fromInteger n)
 
 prop_add :: Positive Double -> Positive Double -> Property
 prop_add (Positive x) (Positive y) = monadicIO $ do
   value <- run $ evalAnyWay $ concat ["(+ ", show x, " ", show y, ")"]
   res <- liftIO . unwrapIOThrows . liftThrows $ unwrapNumber value
-  assert (abs res - (x + y) < eps)
+  assert (res == x + y)
 
 prop_mul :: Positive Double -> Positive Double -> Property
 prop_mul (Positive x) (Positive y) = monadicIO $ do
   value <- run $ evalAnyWay $ concat ["(* ", show x, " ", show y, ")"]
   res <- liftIO . unwrapIOThrows . liftThrows $ unwrapNumber value
-  assert (abs res - (x * y) < eps)
+  assert (res == x * y)
+
+prop_higherOrder :: SmallInt -> Property
+prop_higherOrder n = monadicIO $ do
+  value <- run $ evalAnyWay $ concat ["(((lambda (a) ((lambda (b) (b b)) (lambda (b) (a (lambda (c) ((b b) c)))))) (lambda (f) (lambda (n) (if (&& (< (- n 1) 0.1) (< (- 1 n) 0.1)) 1 (* n (f (- n 1))))))) ", show (unWrap n), ")"]
+  res <- liftIO . unwrapIOThrows . liftThrows $ unwrapNumber value
+  assert (res == fromInteger (factorial (unWrap n))) where
+    factorial n = if n < 2 then 1 else n * factorial (n - 1)
 
 main = do
   quickCheck prop_number
   quickCheck prop_add
   quickCheck prop_mul
+  quickCheck prop_higherOrder
