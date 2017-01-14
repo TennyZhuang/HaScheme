36a11e7f7090b00fca8619b7d15d451cd0798d4a
Songzhou Yang <yangsongzhou@outlook.com>
2017-01-14 14:27:13 +0800

    add pretty-printer

--- src/Repl.hs
+++ src/Repl.hs
@@ -27,10 +27,63 @@ showRes res = do
   putStrLn res
   setSGR [Reset]
 
-showAST :: String -> String
+showAST :: String -> InputT IO ()
 showAST expr = case parse parseExpr "Scheme" expr of
-  Left err -> "No match: " `mappend` show err
-  Right ast -> show ast
+  Left err -> outputStrLn $ "No match: " `mappend` show err
+  Right ast -> liftIO $ showExpr 0 ast
+
+showExpr :: Int -> Expr -> IO ()
+showExpr n ast = do
+  putStr $ replicate n ' '
+  case ast of
+    FuncCallExpr x y -> do
+      setSGR [SetColor Foreground Vivid Blue]
+      putStrLn "FuncCallExpr"
+      setSGR [Reset]
+      showExpr (n + 2) x
+      showExpr (n + 2) y
+    LambdaFuncExpr ss x -> do
+      setSGR [SetColor Foreground Vivid Yellow]
+      putStrLn $ "LambdaFuncExpr " `mappend` show ss
+      setSGR [Reset]
+      showExpr (n + 2) x
+    DefineVarExpr s x -> do
+      setSGR [SetColor Foreground Vivid Green]
+      putStrLn $ "DefineVarExpr " `mappend` show s
+      setSGR [Reset]
+      showExpr (n + 2) x
+    SetVarExpr s x -> do
+      setSGR [SetColor Foreground Vivid Magenta]
+      putStrLn $ "SetVarExpr " `mappend` show s
+      setSGR [Reset]
+      showExpr (n + 2) x
+    ConsExpr (x, y) -> do
+      setSGR [SetColor Foreground Vivid Cyan]
+      putStrLn "ConsExpr"
+      showExpr (n + 2) x
+      showExpr (n + 2) y
+      setSGR [Reset]
+    IfExpr x y z -> do
+      setSGR [SetColor Foreground Vivid Blue]
+      putStrLn "IfExpr"
+      showExpr (n + 2) x
+      showExpr (n + 2) y
+      showExpr (n + 2) z
+      setSGR [Reset]
+    ListExpr xs -> do
+      setSGR [SetColor Foreground Vivid Yellow]
+      putStrLn "ListExpr"
+      setSGR [Reset]
+      showExpr (n + 2) $ AST.fromList xs
+    BeginExpr x -> do
+      setSGR [SetColor Foreground Vivid Yellow]
+      putStrLn "BeginExpr"
+      setSGR [Reset]
+      showExpr (n + 2) x
+    _ -> do
+      setSGR [SetColor Foreground Vivid Cyan]
+      print ast
+      setSGR [Reset]
 
 evalAndPrint :: Environment -> String -> IO ()
 evalAndPrint env expr = case parse parseExpr "Scheme" expr of
@@ -61,7 +114,7 @@ runRepl = let
       Just ":q" -> liftIO exitSuccess
       Just ":t" -> do
         history <- getHistory
-        outputStrLn . showAST . head . tail $ historyLines history -- Error handler
+        showAST . head . tail $ historyLines history -- Error handler
       Just input -> liftIO $ evalAndPrint env input
     loop env
   in runInputT defaultSettings (liftIO builtInEnv >>= loop)
